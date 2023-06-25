3e13ae7b9fda7de8384bf3e194559fb0da1fa510
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-06-25 16:37:50 +0200

    Middle of the way to acomplish task
    About 70 % of funcionalities

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const container = document.querySelector("#container")
+const resetBtn = document.querySelector("#reset")
+let qnt = window.prompt('How many?')
 
-for(let i = 0; i < 16;i++){
-    let div = document.createElement("div")
-    container.appendChild(div)
-    div.textContent = i
+resetBtn.addEventListener('click', () => {
+    
+    qnt = window.prompt('How many?')
+    drawGrid()
+})
+    
+drawGrid()
+function drawGrid(){
+
+    for(let i = 0; i < qnt;i++){
+        let div = document.createElement("div")
+        container.appendChild(div)
+        div.classList.add('grid-element')
+        div.textContent = i
+    }
 }
+
+let allGrids = document.querySelectorAll('.grid-element')
+allGrids.forEach( (x) => {
+    x.addEventListener('mouseover', function(e){
+        this.style.backgroundColor = "green"
+        console.log(e.type)
+    })
+})
