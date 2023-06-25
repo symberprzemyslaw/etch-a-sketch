323a2ae124acf536f2caed47ac592dc525210346
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-06-25 22:56:52 +0200

    Main funconalityy finally works
    Added color picker

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const container = document.querySelector("#container")
 const resetBtn = document.querySelector("#reset")
+const colorInput = document.querySelector("#color")
 let qnt = window.prompt('How many?')
 
 resetBtn.addEventListener('click', () => {
     
     qnt = window.prompt('How many?')
+    container.innerHTML = ''
     drawGrid()
 })
     
 drawGrid()
 function drawGrid(){
 
-    for(let i = 0; i < qnt;i++){
+    for(let i = 0; i < (qnt * qnt);i++){
         let div = document.createElement("div")
         container.appendChild(div)
         div.classList.add('grid-element')
-        div.textContent = i
+        div.style.width = `${900 /qnt}px`
+        //div.style.height = `${500 / qnt}px`
+        //div.textContent = i + 1
     }
+    let allGrids = document.querySelectorAll('.grid-element')
+    allGrids.forEach( (x) => {
+        x.addEventListener('mouseover', function(e){
+            this.style.backgroundColor = colorInput.value
+        })
+    })
 }
 
-let allGrids = document.querySelectorAll('.grid-element')
-allGrids.forEach( (x) => {
-    x.addEventListener('mouseover', function(e){
-        this.style.backgroundColor = "green"
-        console.log(e.type)
-    })
-})
