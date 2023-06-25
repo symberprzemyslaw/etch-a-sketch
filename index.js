6b39ad22e6e717b2f7edfb2599141d623310c6a0
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-06-25 14:51:03 +0200

    Initialization and basic setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container")
+
+for(let i = 0; i < 16;i++){
+    let div = document.createElement("div")
+    container.appendChild(div)
+    div.textContent = i
+}
