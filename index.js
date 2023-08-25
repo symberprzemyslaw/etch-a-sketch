ae0c965f8d36236e7c33418ddca43549efc5619c
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-08-25 14:42:50 +0200

    Updating

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const container = document.querySelector("#container")
+const canvas = document.querySelector("#canvas")
 const resetBtn = document.querySelector("#reset")
 const colorInput = document.querySelector("#color")
-let qnt = window.prompt('How many?')
+let qnt;
 
+
+let getQnt = () => {
+    qnt = window.prompt('How many cubes? (100 max)')
+    if(qnt > 100){
+        qnt = 100
+    }
+    console.log(qnt)
+}
+
+getQnt()
+let canDraw = true
+
+
+document.addEventListener('keydown',e => {
+    e.key === 'Shift' ? canDraw = false : null
+})
+document.addEventListener('keyup',e => {
+    e.key === 'Shift' ? canDraw = true : null
+})
 resetBtn.addEventListener('click', () => {
-    
-    qnt = window.prompt('How many?')
-    container.innerHTML = ''
+    canvas.innerHTML = ''
+    getQnt()
     drawGrid()
 })
     
@@ -15,16 +33,14 @@ function drawGrid(){
 
     for(let i = 0; i < (qnt * qnt);i++){
         let div = document.createElement("div")
-        container.appendChild(div)
+        canvas.appendChild(div)
         div.classList.add('grid-element')
-        div.style.width = `${900 /qnt}px`
-        //div.style.height = `${500 / qnt}px`
-        //div.textContent = i + 1
+        div.style.width = `${700 /qnt}px`
     }
     let allGrids = document.querySelectorAll('.grid-element')
     allGrids.forEach( (x) => {
         x.addEventListener('mouseover', function(e){
-            this.style.backgroundColor = colorInput.value
+            canDraw ? this.style.backgroundColor = colorInput.value :  null
         })
     })
 }
